220bfb1405b951dbc6d1c93e91836fa6fc2b5f9d
AlessioSavi <alessiosavibtc@gmail.com>
2019-07-12 21:56:39 +0200

    Free the pre-allocated memory (the two string used in C.verify_presence_data_insensitive(C.CString(data), C.CString(to_find_lower)) directly from C

--- cutils.c
+++ cutils.c
@@ -4,22 +4,21 @@
 #include "cutils.h"
 #include <ctype.h>
 
-/* This metod is delegated to init the fsize in input with the byte lenght of the given file */
-void set_file_size(FILE *fp, int *fsize)
-{  
+/* This metod is delegated to init the fsize in input with the byte lenght of
+ * the given file */
+void set_file_size(FILE *fp, int *fsize) {
   fseek(fp, 0, SEEK_END);
   *fsize = ftell(fp);
   rewind(fp);
   return;
 }
 
-long get_file_size(char *filename)
-{  
+long get_file_size(char *filename) {
   long fsize = 0;
   FILE *fp;
 
   fp = fopen(filename, "r");
-  if(fp) {
+  if (fp) {
     fseek(fp, 0, SEEK_END);
     fsize = ftell(fp);
     fclose(fp);
@@ -27,18 +26,16 @@ long get_file_size(char *filename)
   return fsize;
 }
 
-
 /* Read the file and return the content */
-char *read_content(const char *filename)
-{
+char *read_content(const char *filename) {
   char *fcontent = NULL;
   int fsize = 0;
   FILE *fp;
   fp = fopen(filename, "r");
-  if(fp) {
+  if (fp) {
     set_file_size(fp, &fsize);
-    printf("File size: %d bytes\n",fsize);
-    fcontent = (char*) malloc(sizeof(char) * fsize);
+    printf("File size: %d bytes\n", fsize);
+    fcontent = (char *)malloc(sizeof(char) * fsize);
     fread(fcontent, 1, fsize, fp);
     fclose(fp);
   }
@@ -46,82 +43,76 @@ char *read_content(const char *filename)
 }
 
 /* Read the file and return the content */
-void read_content_no_alloc(const char *filename, char *out)
-{
+void read_content_no_alloc(const char *filename, char *out) {
   int fsize = 0;
   FILE *fp;
   fp = fopen(filename, "r");
-  if(fp) {
+  if (fp) {
     set_file_size(fp, &fsize);
     fread(out, 1, fsize, fp);
     fclose(fp);
   }
-  return ;
+  return;
 }
 
 // Lower the case of the string in input
-void lowerize_string(char *data){
+void lowerize_string(char *data) {
   int i = 0; // Don't break c99 :/i
-  for(; data[i]; i++){
+  for (; data[i]; i++) {
     data[i] = tolower(data[i]);
-  } 
+  }
 }
 
-
-int verify_presence_data_insensitive(char *data, const char *to_find)
-{
+int verify_presence_data_insensitive(char *data, char *to_find) {
+  int ret = -1;
   /* Manage None input */
-  if(data != NULL && to_find != NULL){
-
+  if (data != NULL && to_find != NULL) {
+    // printf("Data: %s | ToFind: %s\n",data,to_find);
     // lowering the line
     lowerize_string(data);
     /* Finding string ... */
-    if(strstr(data,to_find)!=NULL)
-    {
-      // printf("String %s found ..\n",to_find);
-      return 1;
+    if (strstr(data, to_find) != NULL) {
+      // printf("String %s found ..\n", to_find);
+      ret = 1;
+    } else {
+      // printf("String %s NOT found ..\n", to_find);
+      ret = 0;
     }
-    //printf("String %s NOT found ..\n",to_find);
-    return 0; 
   }
-  else
-    // Error - input
-    return -1;
+
+  free(data);
+  free(to_find);
+  return ret;
 }
 
-
-int verify_presence_data(const char *data, const char *to_find)
-{
+int verify_presence_data(const char *data, const char *to_find) {
   /* Manage None input */
-  if(data != NULL && to_find != NULL){
+  if (data != NULL && to_find != NULL) {
     /* Finding string ... */
-    if(strstr(data,to_find)!=NULL)
-    {
+    if (strstr(data, to_find) != NULL) {
       // printf("String %s found ..\n",to_find);
       return 1;
     }
-    //printf("String %s NOT found ..\n",to_find);
-    return 0; 
-  }
-  else
+    // printf("String %s NOT found ..\n",to_find);
+    return 0;
+  } else
     // Error - input
     return -1;
 }
 
-int verify_presence_filename(const char *filename, const char *to_find)
-{
+int verify_presence_filename(const char *filename, const char *to_find) {
 
   /* Manage None input */
-  if(filename != NULL && to_find != NULL){
+  if (filename != NULL && to_find != NULL) {
 
     const char *content;
-    content =(char *) read_content(filename);
+    content = (char *)read_content(filename);
 
     /* Finding string ... */
-    return verify_presence_data(content,to_find);
+    return verify_presence_data(content, to_find);
   }
 
-  printf("Seems that the filename or the string to find is NULL :/ ..."); 
+  printf("Seems that the filename or the string to find is NULL :/ ...");
   return -1;
 }
 
@@ -134,6 +125,4 @@ int verify_presence_filename(const char *filename, const char *to_find)
   verify_presence_filename(content,"filename.txt");
   free(content);
   return 0;
-  }*/   
-
-
+  }*/
