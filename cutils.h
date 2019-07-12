36bd9288e52fb3469318cc0bcab0df5d69cfd6dd
AlessioSavi <alessio.savi-cic-it@ibm-com>
2019-07-12 21:56:39 +0200

    Free the pre-allocated memory (the two string used in C.verify_presence_data_insensitive(C.CString(data), C.CString(to_find_lower)) directly from C

--- cutils.h
+++ cutils.h
@@ -6,12 +6,13 @@
 char *read_content(const char *filename);
 void read_content_no_alloc(const char *filename, char *out);
 
-long get_file_size(char * filename);
+long get_file_size(char *filename);
 
 /* Verify that 'to_find' is present in the given data */
 int verify_presence_data(const char *data, const char *to_find);
 
-/* Wrap the read_content method and verify if 'to_find' is in the content of the given file */
+/* Wrap the read_content method and verify if 'to_find' is in the content of the
+ * given file */
 int verify_presence_filename(const char *filename, const char *to_find);
 
 /* Set the the file size in byte into the fsize pointer in input */
@@ -20,9 +21,7 @@ void set_file_size(FILE *fp, int *fsize);
 /* Lowerize the given string */
 void lowerize_string(char *data);
 
-
 /* Lowerize the input 'data' and verify if 'to_find' is present */
-int verify_presence_data_insensitive(char *data, const char *to_find);
-
+int verify_presence_data_insensitive(char *data, char *to_find);
 
 #endif
