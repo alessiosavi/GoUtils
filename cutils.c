be260ae2a5461b5592dba8d425c69d36c0d46af9
AlessioSavi <alessiosavibtc@gmail.com>
2019-08-11 13:39:53 +0200

    - Create golang wrapper method for interact with C spell check function [@@ -698,3 +697,14 @@ func SecureRequest(ctx *fasthttp.RequestCtx, ssl bool)] - Create method for manage spell correctin (levenshtein_n and levenshtein from https://github.com/wooorm/levenshtein.c, apply_levian_to_array, split_data, spell_check) [@@ -123,22 +124,187 @@ static char **makeCharArray(int size)] - Fix headers files [ @@ -1,27 +1,28 @@]

--- cutils.c
+++ cutils.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include "cutils.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <ctype.h>
+#include "cutils.h"
 
 /* This metod is delegated to init the fsize in input with the byte lenght of
  * the given file */
@@ -34,7 +35,7 @@ char *read_content(const char *filename) {
   fp = fopen(filename, "r");
   if (fp) {
     set_file_size(fp, &fsize);
-    printf("File size: %d bytes\n", fsize);
+    //printf("File size: %d bytes\n", fsize);
     fcontent = (char *)malloc(sizeof(char) * fsize);
     fread(fcontent, 1, fsize, fp);
     fclose(fp);
@@ -123,21 +124,186 @@ static char **makeCharArray(int size) { return calloc(sizeof(char *), size); }
 
 static void setArrayString(char **a, char *s, int n) { a[n] = s; }
 
-static void freeCharArray(char **a, int size) {
+
+size_t levenshtein_n(const char *a, const size_t length, const char *b,
+                     const size_t bLength) {
+  size_t index = 0;
+  size_t bIndex = 0;
+  size_t bDistance;
+  size_t result;
+
+  // Shortcut optimizations / degenerate cases.
+  if (a == b || strcmp(a, b) == 0) {
+    return 0;
+  }
+
+  if (length == 0) {
+    return bLength;
+  }
+
+  if (bLength == 0) {
+    return length;
+  }
+
+  size_t *cache = calloc(length, sizeof(size_t));
+
+  // initialize the vector.
+  while (index < length) {
+    cache[index] = index + 1;
+    ++index;
+  }
+  size_t distance;
+  char code;
+
+  // Loop.
+  while (bIndex < bLength) {
+    code = b[bIndex];
+    result = distance = bIndex++;
+    index = SIZE_MAX;
+
+    while (++index < length) {
+      bDistance = code == a[index] ? distance : distance + 1;
+      distance = cache[index];
+
+      cache[index] = result = distance > result ? bDistance > result
+          ? result + 1
+          : bDistance
+          : bDistance > distance ? distance + 1 : bDistance;
+    }
+  }
+  // printf("\nDistance from [%s] and [%s] is: [%zu]", a, b, result);
+  free(cache);
+
+  return result;
+}
+
+int levenshtein(const char *a, const char *b) {
+  const size_t length = strlen(a);
+  const size_t bLength = strlen(b);
+
+  return (int)levenshtein_n(a, length, b, bLength);
+}
+
+// C helper functions:
+
+void freeCharArray(char **a, int size) {
   int i;
-  for (i = 0; i < size; i++)
-    free(a[i]);
+  for (i = 0; i < size; i++) {
+    if (a[i] != NULL) {
+      printf("\nFreeing [%s]...", a[i]);
+      free(a[i]);
+    } else {
+      printf("\n Unable to free!");
+    }
+  }
   free(a);
+}
+
+char *apply_levian_to_array(char **a, char *wrong, int size) {
+  int i;
+  int *ret_arr = malloc(sizeof(int) * size);
+  char *correct = NULL;
+  //printf("\napply_levian_to_array | START | Lenght: %d", size);
+  for (i = 0; i < size; i++) {
+    ret_arr[i] = levenshtein(a[i], wrong);
+    if (ret_arr[i] == 1) {
+      //printf("\nFOUND: [%s]", a[i]);
+      free(ret_arr);
+      return a[i];
+    }
+  }
+
+  char alredy_found = '0';
+  //printf("\n TRESHOLD: 2");
+  for (i = 0; i < size; i++) {
+    if (ret_arr[i] == 2) {
+      //printf("\nFOUND: [%s]", a[i]);
+      correct = a[i];
+      alredy_found = '1';
+      break;
+    }
+  }
+  if (alredy_found == '0') {
+    //printf("\n TRESHOLD: 3");
+    for (i = 0; i < size; i++) {
+      if (ret_arr[i] == 3) {
+        //printf("\nFOUND: [%s]", a[i]);
+        correct = a[i];
+        alredy_found = '1';
+        break;
+      }
+    }
+  }
+  if (alredy_found == '0') {
+    //printf("\n TRESHOLD: 4");
+    for (i = 0; i < size; i++) {
+      if (ret_arr[i] == 4) {
+        //printf("\nFOUND: [%s]", a[i]);
+        correct = a[i];
+        alredy_found = '1';
+        break;
+      }
+    }
+  }
+ // printf("\napply_levian_to_array | STOP\n");
+
+  free(ret_arr);
+  return correct;
 }
 
 static void printCharArray(char **a, int size) {
   int i;
   printf("\nprintCharArray | START | Lenght: %d", size);
-  for (i = 0; i < size; i++)
+  for (i = 0; i < size; i++) {
     printf("\n%d) %s", i, (a[i]));
+  }
   printf("\nprintCharArray | STOP\n");
   return;
 }
+
+char **split_data(char *str, int lenght) {
+
+  char *saveptr;
+  char **array = malloc(sizeof(char *) * lenght);
+  char delim[] = "\n";
+  int i = 0;
+  char *ptr = strtok_r(str, delim, &saveptr);
+  // printf("'%s'\n", ptr);
+  array[i] = ptr;
+  while ((ptr = strtok_r(NULL, delim, &saveptr)) != NULL) {
+    array[++i] = ptr;
+    //    printf("'%s'\n", ptr);
+  }
+  free(ptr);
+//  printf("\n");
+  return array;
+}
+
+
+char *spell_check(const char *filename, const int file_lines, char *wrong) {
+  //printf("\nSTART | spell_check");
+  char *dict = read_content(filename);
+
+  char **array = split_data(dict, file_lines);
+  // printCharArray(array,102264);
+  char *correct = apply_levian_to_array(array, wrong, file_lines);
+  printf("\nCorrect substitution for [%s] is [%s]\n", wrong, correct);
+
+  //free(dict);
+  free(array);
+  //printf("\nSTOP | spell_check");
+  return correct;
+}
+
+/*
+int main() {
+  const int dict_size = 102264;
+  char *wrong = "zeccchin";
+  char *right =  spell_check("all",dict_size,wrong);
+  printf("\nSpell: %s",right);
+  return 0;
+}
+*/
 
 /*int main(int argc, char **argv)
   {
